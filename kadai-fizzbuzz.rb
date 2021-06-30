1f9c1dab71cddd40ffaa8ee459b5e35d7ad5ed17
taichi.naitou <taisan1313@ezweb.ne.jp>
2021-06-30 06:49:17 +0000

    FizzBuzz

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,15 @@
+def FizzBuzz(num)
+    if num % 15 == 0
+        return 'FizzBuzz'
+    elsif num % 3 ==0
+        return 'Fizz'
+    elsif num % 5 ==0
+        return 'Buzz'
+    else
+        return num
+    end
+end
+num_max = 100
+(1..num_max).each do |number|
+    puts FizzBuzz(number)
+end
